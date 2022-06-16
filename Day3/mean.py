98332f0908fd89579609c2d41760d0bd66a1a9d6
calistaagmata <92999047+calistaagmata@users.noreply.github.com>
2022-06-16 11:31:27 -1000

    Update mean.py

--- Day3/mean.py
+++ Day3/mean.py
@@ -11,21 +11,21 @@ import random
 
 def main(): 
 
-# prompt the user for a random amount of ratings 
-amount_stars = random.randint(5,10) 
-print("Input", amount_stars, "ratings") 
-stars = input("Input ratings sepreated by commas:") 
+    # prompt the user for a random amount of ratings 
+    amount_stars = random.randint(5,10) 
+    print("Input", amount_stars, "ratings") 
+    stars = input("Input ratings sepreated by commas:") 
 
-# use list comprehension to turn user input to a list 
-stars = stars.split(",") 
-stars = float(star) for star in stars] 
+    # use list comprehension to turn user input to a list 
+    stars = stars.split(",") 
+    stars = float(star) for star in stars] 
 
-# calculate the mean rounded to one decimal point 
-total = 0 
-divisor = len(stars) 
-for star in stars: 
-    total = total + star 
-mean = round(total/divisor, 1) 
+    # calculate the mean rounded to one decimal point 
+    total = 0 
+    divisor = len(stars) 
+    for star in stars: 
+        total = total + star 
+    mean = round(total/divisor, 1) 
 
-# give user the mean 
-print("The mean of the ratings for Harry's restaurant is", mean) 
+    # give user the mean 
+    print("The mean of the ratings for Harry's restaurant is", mean) 
